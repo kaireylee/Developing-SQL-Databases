6032a4e6728e01735fdaf22abf5540d5ce79711a
xants <kairey@gmail.com>
2019-05-06 12:03:51 -0700

    Guids

--- 02-TableDesign/Module2-TableDesign/Guids.sql
+++ 02-TableDesign/Module2-TableDesign/Guids.sql
@@ -1,3 +1,18 @@
 Use LabData;
 Go
 
+--GUID global unique identifier values are not equivalent anywhere else
+
+Create Table GuidTable
+(
+	RandomGuid UniqueIdentifier Default NewID(),
+	SequentialGuid UniqueIdentifier Default NewSequentialID(),
+	Content varchar(50)
+);
+
+Insert Into GuidTable Default Values;
+Insert Into GuidTable Default Values;
+
+Select * From GuidTable
+
+Drop Table GuidTable
