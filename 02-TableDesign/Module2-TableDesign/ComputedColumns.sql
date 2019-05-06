492eabb98cd82248f33b594819c449b28d80112f
xants <kairey@gmail.com>
2019-05-06 14:35:15 -0700

    Computed Columns and Schemas

--- 02-TableDesign/Module2-TableDesign/ComputedColumns.sql
+++ 02-TableDesign/Module2-TableDesign/ComputedColumns.sql
@@ -1,3 +1,21 @@
 Use LabData;
 Go
 
+Begin Try
+	Create Table Employee (
+		EmployeeId int,
+		FirstName varchar(25),
+		LastName varchar(25), --Persisted = updates when the data is queried for
+		FullName as (FirstName + ' ' + LastName) Persisted --Type is based on the subtypes of the data being computed
+	);
+
+	Insert Into Employee (EmployeeId, FirstName, LastName) Values (1 ,'Robert', 'Baratheon');
+	Insert Into Employee (EmployeeId, FirstName, LastName) Values (2, 'Tywin', 'Lannister');
+
+	Select * From Employee; 
+
+	Drop Table Employee; 
+End Try
+Begin Catch
+	Print(Error_Message());
+End Catch
--- 02-TableDesign/Module2-TableDesign/Schemas.sql
+++ 02-TableDesign/Module2-TableDesign/Schemas.sql
@@ -16,3 +16,27 @@ Create Table Marketing.[Events] --Brackts around Reserved Words
 (
 	MarketingId int
 ); 
+
+Create Table Sales.Customers
+(
+	SalesId int
+);
+
+--Transfer General Table to Sales Schema
+exec sp_tables;
+Alter Schema Sales Transfer dbo.General
+exec sp_tables;
+
+Begin Try 
+	Drop Table Sales.Customers;
+	Drop Table Sales.General;
+	Drop Table Marketing.Events;
+
+	Drop Schema Sales;
+	Drop Schema Marketing;
+	Print('Schemas Dropped')
+End Try
+Begin Catch
+	Print(Error_Message());
+End Catch
+
