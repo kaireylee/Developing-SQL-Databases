36690d83810de6749138fc6d9595ed55231b9725
xants <kairey@gmail.com>
2019-05-09 10:05:12 -0700

    Memory Optimized Tables

--- 12-memoryoptdata/12-MemoryOptData/InMemoryData.sql
+++ 12-memoryoptdata/12-MemoryOptData/InMemoryData.sql
@@ -2,24 +2,34 @@ Use Master;
 Go
 
 Create DataBase MemoryOptDB;
-
 --------------------
 -- File Groups
 --------------------
+Alter Database MemoryOptDB
+	Add FileGroup Memory_Data
+	Contains Memory_Optimized_Data
+
+Alter Database MemoryOptDB
+	Add File (Name='MemData', FIlename='C:\Data\MemoryOptDB_MemData.ndf')
+	To FileGroup Memory_Data;
+Go
 
 ------------------------------
 --InMemory Table
 ------------------------------
 Use MemoryOptDB;
 Go
-Create TAble InMemoryTable
+Create Table InMemoryTable
 (
 	OrderID int identity(1,1),
 	OrderDate DateTime Not null,
 	Quantity int,
-	Price smallmoney
+	Price smallmoney,
+	Primary Key NonClustered (OrderId)
+)
+With (Memory_Optimized=On, Durability=Schema_And_Data);
 
-);
+
 Go
 
 Insert Into InMemoryTable Values(GetDate(),10,100.00);
