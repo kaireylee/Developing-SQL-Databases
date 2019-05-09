bfe23dc811194dd9cc6f8852628e71cbe5379d32
xants <kairey@gmail.com>
2019-05-09 10:48:17 -0700

    Alter Buffer Pool
    Utilizes SSD's to add more room for buffering

--- 12-memoryoptdata/12-MemoryOptData/AlterBufferPool.sql
+++ 12-memoryoptdata/12-MemoryOptData/AlterBufferPool.sql
@@ -1,9 +1,17 @@
 USE MASTER;
 GO
 --------------------------------------------
---Add MORE room for Buffering to an SSC 
+--Add MORE room for Buffering to an SSD
 --  NOT Useful for In-Memory Tables
+--RAM Memory is more expensive than SSD memory 
 ---------------------------------------------
+ALTER SERVER CONFIGURATION
+	SET BUFFER POOL Extension OFF; 
+
+ALTER SERVER CONFIGURATION
+	SET BUFFER POOL Extension ON
+	(FILENAME='C:\Data\MyCache.bpe', Size=50 GB);
+
 
 ------------------------------------------------
 --To reconfigure Turn off then ALTER again
