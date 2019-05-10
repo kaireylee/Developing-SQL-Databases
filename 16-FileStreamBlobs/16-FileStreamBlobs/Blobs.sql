fa48e6334ffc8f43cae31973922f574dae6baafa
xants <kairey@gmail.com>
2019-05-10 09:46:47 -0700

    Image blob

--- 16-FileStreamBlobs/16-FileStreamBlobs/Blobs.sql
+++ 16-FileStreamBlobs/16-FileStreamBlobs/Blobs.sql
@@ -1,3 +1,35 @@
-USE master;
+USE Northwind;
 GO
 
+CREATE DATABASE FileStreamDM; 
+
+EXEC sp_configure Filestream_Access_Level, 1;
+RECONFIGURE;
+
+USE FileStreamDB;
+GO
+
+ALTER DATABASE FileStreamDB
+	ADD FILEGROUP Primary_Filestream CONTAINS FILESTREAM;
+
+GO
+
+ALTER DATABASE FileStreamDB
+	ADD File(Name='MyBlobs', Filename='C:\Data\MyFilestream_Blobs')
+		TO FILEGROUP Primary_Filestream
+
+GO
+
+CREATE TABLE Images
+(
+	ID UNIQUEIDENTIFIER ROWGUIDCOL NOT NULL UNIQUE,
+	ImageFile VARBINARY(MAX) FILESTREAM
+);
+
+GO
+
+INSERT INTO Images(Id, ImageFile)
+	SELECT NewID(), BulkColumn
+	FROM OpenRowSet(Bulk 'C:\Developing SQL Databases\Images\test.jpg', single_blob) AS F; 
+
+SELECT * FROM Images;
